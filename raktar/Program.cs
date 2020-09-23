67d649573b173a48875fed2c7f55683f3d17bd06
FabianTamas <fabiant0130@gmail.com>
2020-09-23 14:56:15 +0200

    Beolvasás elkezdése

--- raktar/Program.cs
+++ raktar/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace raktar
 {
     class Program
     {
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                /*
+                 * sor[0] -> Kód
+                 * sor[1] -> Név
+                 * sor[2] -> Ár
+                 * sor[3] -> Darabszám
+                 */
+                string[] sor = raktar.ReadLine().Split(';');
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
+            BeolvasRaktar();
 
             Console.ReadKey();
         }
